25c8c7a705e32753b2c6fbea3d0e935d5dc26669
sasidhy1 <sasidhy1@gmail.com>
2018-10-30 02:51:11 -0400

    Clean up population table and datatypes, rename columns and drop empty rows.

--- cleanup/etl_population.sql
+++ cleanup/etl_population.sql
@@ -3,9 +3,11 @@ USE etl_project;
 
 SELECT * FROM population LIMIT 25;
 
+-- add primary key
 ALTER TABLE population
 ADD id INT AUTO_INCREMENT PRIMARY KEY FIRST;
 
+-- rename columns of interest
 ALTER TABLE population
 CHANGE `Geographic Area` geo_area text,
 CHANGE `2010` y_2010 text,
@@ -19,6 +21,7 @@ CHANGE `2016` y_2016 text;
 -- turn off safe updates
 SET SQL_SAFE_UPDATES = 0;
 
+-- remove commas from str numbers
 UPDATE population
 SET geo_area = REPLACE(geo_area,'.',''),
 	y_2010 = REPLACE(y_2010,',',''),
@@ -32,6 +35,7 @@ SET geo_area = REPLACE(geo_area,'.',''),
 -- check how many entries
 SELECT COUNT(*) FROM population;
 
+-- remove empty entries
 DELETE FROM population
 WHERE geo_area = '';
 
@@ -41,13 +45,14 @@ SELECT COUNT(*) FROM population;
 -- turn on safe updates again
 SET SQL_SAFE_UPDATES = 1;
 
+-- convert columns to int datatype
 ALTER TABLE population
-MODIFY COLUMN y_2010 INT,
-MODIFY COLUMN y_2011 INT,
-MODIFY COLUMN y_2012 INT,
-MODIFY COLUMN y_2013 INT,
-MODIFY COLUMN y_2014 INT,
-MODIFY COLUMN y_2015 INT,
-MODIFY COLUMN y_2016 INT;
+	MODIFY COLUMN y_2010 INT,
+	MODIFY COLUMN y_2011 INT,
+	MODIFY COLUMN y_2012 INT,
+	MODIFY COLUMN y_2013 INT,
+	MODIFY COLUMN y_2014 INT,
+	MODIFY COLUMN y_2015 INT,
+	MODIFY COLUMN y_2016 INT;
 
 SELECT * FROM population LIMIT 25;
